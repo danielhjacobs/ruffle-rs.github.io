7382b0455a8ee4dcd95cdaa0e01c768d30d0da25
Daniel Jacobs <danielhunterjacobs@gmail.com>
2025-06-17 09:38:17 -0400

    Fix #210

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,14 @@
 
 import classes from "./not-found.module.css";
 import { Stack, Text, Title } from "@mantine/core";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (document.location.pathname === "/blog/") {
+      document.location.href = "/blog";
+    }
+  }, []);
   return (
     <Stack align="center">
       <Title className={classes.title}>404</Title>
